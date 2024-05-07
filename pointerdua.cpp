c85c7cdd09ccbd9135cc3fbe002ae957841b0ea9
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-07 15:40:13 +0700

    menambahkan metode setnama dalam kelas mahasiswa

--- pointerdua.cpp
+++ pointerdua.cpp
@@ -1,7 +1,12 @@
-#include <iostream>\
+#include <iostream>
 using namespace std;
 
 class mahasiswa 
 {
-
+public:
+    int nim;
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+    }    
 };
