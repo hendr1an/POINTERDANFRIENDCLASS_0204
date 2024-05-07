8975cc9b57f748d6725389c002f811c26e9720df
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-05-07 15:15:43 +0700

    Menyelesaikan class segitiga

--- contoh.cpp
+++ contoh.cpp
@@ -25,3 +25,19 @@ public:
         return p.panjang;
     }
 };
+
+int main(){
+    persegipanjang pp;
+    segitiga sg;
+    cout << "Masukkan Panjang : ";
+    cin >> pp.panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> pp.lebar;
+    cout << "Masukkan Alas : ";
+    cin >> sg.alas;
+    cout << "Masukkan Tinggi : ";
+    cin >> sg.tinggi;
+    cout << "luas persegi panjang : " << pp.luasPersegiPanjang(pp.panjang, pp.lebar) << endl;
+    cout << "luas segitga : " << sg.luasSegitiga(sg.alas, sg.tinggi) << endl;
+    cout << "panjang persegi panjang : " << sg.hitung(pp) << endl;
+}
